c38e64f7d49fd87d6ffaac320e25126862353717
AlekseyTrof <alekcey.trofimov.99@gmail.com>
2025-01-08 14:38:06 +0300

    added tasks from lesson2.3

---
+++ src/skyPro_course2/lesson3/Bicycle.java
@@ -0,0 +1,9 @@
+package skyPro_course2.lesson3;
+
+public class Bicycle extends Transports {
+
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+}
---
+++ src/skyPro_course2/lesson3/Service.java
@@ -0,0 +1,7 @@
+package skyPro_course2.lesson3;
+
+public interface Service {
+
+    void check(Transports transport);
+
+}
